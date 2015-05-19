b12e9c5ee560025fbf90ede563e975f0ac5e0f80
Benedict Cohen <ben@benedictcohen.co.uk>
2015-05-19 10:52:16 +0100

    - Added iOS Static library target for Deploymate checking

---
+++ iOSBCLRedirectingURLCache/iOSBCLRedirectingURLCache.h
@@ -0,0 +1,13 @@
+//
+//  iOSBCLRedirectingURLCache.h
+//  iOSBCLRedirectingURLCache
+//
+//  Created by Benedict Cohen on 19/05/2015.
+//  Copyright (c) 2015 Benedict Cohen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface iOSBCLRedirectingURLCache : NSObject
+
+@end
---
+++ iOSBCLRedirectingURLCache/iOSBCLRedirectingURLCache.m
@@ -0,0 +1,13 @@
+//
+//  iOSBCLRedirectingURLCache.m
+//  iOSBCLRedirectingURLCache
+//
+//  Created by Benedict Cohen on 19/05/2015.
+//  Copyright (c) 2015 Benedict Cohen. All rights reserved.
+//
+
+#import "iOSBCLRedirectingURLCache.h"
+
+@implementation iOSBCLRedirectingURLCache
+
+@end
